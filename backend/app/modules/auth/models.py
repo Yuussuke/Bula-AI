e736543579e2ad5e9db93ccadbf97a5bd9e231bb
Yuussuke <yuussuke@gmail.com>
2026-04-27 11:41:08 -0300

    Annotate user role column

--- backend/app/modules/auth/models.py
+++ backend/app/modules/auth/models.py
@@ -27,7 +27,7 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
-    role = Column(
+    role: Column[UserRole] = Column(
         Enum(
             UserRole,
             name="userrole",
